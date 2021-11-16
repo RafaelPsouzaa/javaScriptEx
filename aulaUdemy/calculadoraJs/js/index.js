477e4f9ccc9a4441ec239f15c0dd97e9c7819f91
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-11-16 10:15:38 -0300

    calculadora

---
+++ aulaUdemy/calculadoraJs/js/index.js
@@ -0,0 +1,12 @@
+let calc = {
+    somar:function(n1,n2){
+        return n1+n2;
+    },
+    subtract:function(n1,n2){
+        return n1-n2;
+    },
+    multiply:function(n1,n2){
+        return n1*n2;
+    }
+}
+console.log(calc.multiply(2,2));
