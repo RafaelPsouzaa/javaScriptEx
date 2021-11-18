abb3d34df0da53f4151e7860d27047ffac75f86d
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-11-18 15:23:44 -0300

    ok

--- aulaUdemy/objetoComClass/js/index.js
+++ aulaUdemy/objetoComClass/js/index.js
@@ -1,11 +1,20 @@
-class Cachorro{
-    constructor(raca,pata,cor){
-        this.race = raca;
-        this.paw = pata;
-        this.color = cor;
+class Mamifero {
+    constructor(patas){
+        this.patas = patas;
     }
-
 }
+let coiote = new Mamifero(4);
+console.log(coiote.patas);
 
-let husky = new Cachorro('HUSKY',2,'BLACK');
-console.log(husky);
+class Cachorro extends Mamifero{
+    constructor(patas,raca){
+        super(patas,patas);
+        this.raca = raca;
+    }
+    latir(){
+        console.log("au auh");
+    }
+}
+let pug = new Cachorro(4,"pug");
+
+console.log(pug);
