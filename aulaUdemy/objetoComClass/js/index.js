3ba109b536d9a971b54748bebff54a97faebd312
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-11-18 14:25:36 -0300

    ok

---
+++ aulaUdemy/objetoComClass/js/index.js
@@ -0,0 +1,11 @@
+class Cachorro{
+    constructor(raca,pata,cor){
+        this.race = raca;
+        this.paw = pata;
+        this.color = cor;
+    }
+
+}
+
+let husky = new Cachorro('HUSKY',2,'BLACK');
+console.log(husky);
