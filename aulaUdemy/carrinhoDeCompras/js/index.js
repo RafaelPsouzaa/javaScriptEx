dbab5cbef1daf6eca1ec47eaf7a814f988a2382b
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-11-26 08:22:33 -0300

    ok

---
+++ aulaUdemy/carrinhoDeCompras/js/index.js
@@ -0,0 +1,11 @@
+class carrinhoCompra {
+    constructor(itens,quantidadeTotal,valorTotal){
+        this.itens = itens;
+        this.quantidadeTotal = quantidadeTotal;
+        this.valorTotal = valorTotal;
+
+    }
+    adicionarItens(addItens){
+        this.adicionarItens + this.itens;
+    }
+}
