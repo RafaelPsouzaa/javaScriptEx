d0ed27676ff3638e7c882efcdf5228dbac1b9e88
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-12-02 16:02:00 -0300

    endereço terminado

--- aulaUdemy/endereçoUpdate/js/index.js
+++ aulaUdemy/endereçoUpdate/js/index.js
@@ -1,6 +1,26 @@
-class endereco {
+class Endereco {
     constructor(rua,cep,bairro){
+        this.rua = rua;
+        this.cep = cep;
+        this.bairro = bairro;
+
+    }
+     set newEndereco(editBairro){
+        this.bairro = editBairro;
+
+    }
+    set newRua(editRua){
+        this.rua = editRua;
+
+    }
+     set newCep(editCep){
+        this.cep = editCep;
 
     }
 
 }
+
+let endereco = new Endereco("rua imip",5643211,"coelhos");
+console.log(endereco.bairro);
+endereco.newEndereco("mangabeira");
+console.log(endereco.bairro);
