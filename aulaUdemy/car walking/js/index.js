161685d77e0a628f66b050fe692434eab5a36599
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-12-06 15:32:54 -0300

    terminado

--- aulaUdemy/car walking/js/index.js
+++ aulaUdemy/car walking/js/index.js
@@ -8,12 +8,19 @@ class Car  {
         this.Consumption = this.gasoline - Consumption;
         this.gasoline = this.Consumption;
     }
+    toFuel(value){
+        this.gasoline += value;
+
+
+    }
 
         
     }
 
     let brasilia = new Car ("Brasilia","yellow",10);
     brasilia.traveledKilometer(2);
+    console.log(brasilia);
+    brasilia.toFuel(100);
     console.log(brasilia);
     
    
