d45c4018aa932943eea676ccc176e5810d0c6038
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-12-21 13:21:13 -0300

    testes regex

--- aulaUdemy/regex/aula2/js/index.js
+++ aulaUdemy/regex/aula2/js/index.js
@@ -4,3 +4,10 @@ console.log(dregex.test("asd"));
 console.log(dregex.test(" "));
 console.log(dregex.test("123"));
 console.log(dregex.test("123asd"));
+const Dregex = /\D/;
+console.log('D');
+console.log(dregex.test("asd"));
+console.log(dregex.test(" "));
+console.log(dregex.test("123"));
+console.log(dregex.test("123asd"));
+
