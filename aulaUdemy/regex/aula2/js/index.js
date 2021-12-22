08d48055ee2026b79a1d4ccc1f39a537d1c49176
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-12-22 09:38:04 -0300

    limite de string

--- aulaUdemy/regex/aula2/js/index.js
+++ aulaUdemy/regex/aula2/js/index.js
@@ -11,3 +11,16 @@ console.log(dregex.test(" "));
 console.log(dregex.test("123"));
 console.log(dregex.test("123asd"));
 
+const dia = /\d\d/;
+console.log("testes mais rigoros");
+console.log(dia.test("2019")&& "2019".length == 2);
+console.log(dia.test("asd"));
+console.log(dia.test("05") && "05".length == 2)
+console.log(dia.test("asd123"));
+
+const palavraTresLetras = /\w\w\w/;
+console.log("testes string 3 letras")
+console.log(palavraTresLetras.test("as"));
+console.log(palavraTresLetras.test("asdxx"));
+console.log(palavraTresLetras.test("asd123"));
+
