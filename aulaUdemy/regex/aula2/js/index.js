c7e2cae760f64a2c2c36961a6c38d65a72c67b17
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-12-23 10:15:11 -0300

    end

--- aulaUdemy/regex/aula2/js/index.js
+++ aulaUdemy/regex/aula2/js/index.js
@@ -11,3 +11,10 @@ console.log(dregex.test(" "));
 console.log(dregex.test("123"));
 console.log(dregex.test("123asd"));
 
+const dia = /\d\d/;
+console.log("dd");
+console.log(dia.test("201955555"));
+console.log(dia.test("1989"));
+console.log(dia.test("05"));
+console.log(dia.test("asd191189"));
+
