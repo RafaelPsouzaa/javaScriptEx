55e45e64f7e0a4c71c6ddd6b8fabd3da345fb873
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-12-27 09:15:34 -0300

    aula4Begin

--- aulaUdemy/regex/aula3/js/index.js
+++ aulaUdemy/regex/aula3/js/index.js
@@ -3,7 +3,7 @@ const notab = /[^ab]/;
 console.log(notab.test("a"));
 console.log(notab.test("aqui tem teste a"));
 
-const nottoaz = /[^a-z]/;
+const notToaz = /[^a-z]/;
 console.log("a-z");
-console.log(nottoaz.test("123"));
-console.log(nottoaz.test("asdflçaskdfjslçakdfkçdjh"));
+console.log(notToaz.test("123"));
+console.log(notToaz.test("asdflçaskdfjslçakdfkçdjh"));
