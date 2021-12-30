9d424caa97dd988f137d9732297a6a126c010d3d
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2021-12-30 08:39:07 -0300

    validando letra maiuscula

---
+++ aulaUdemy/regex/Exercicio/exer01/js/index.js
@@ -0,0 +1,5 @@
+const validarMaiuscula = /[A-Z]/;
+
+console.log(validarMaiuscula.test("VALIDANDO"));
+console.log(validarMaiuscula.test("validando"));
+console.log(validarMaiuscula.test("12345345"));
