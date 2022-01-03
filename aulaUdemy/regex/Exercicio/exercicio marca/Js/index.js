c33ee74ad3ecc355c2dfabfc4e070594a5f27f79
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-01-03 10:03:28 -0300

    validando marcas

--- aulaUdemy/regex/Exercicio/exercicio marca/Js/index.js
+++ aulaUdemy/regex/Exercicio/exercicio marca/Js/index.js
@@ -1,3 +1,6 @@
 const marcas = /marca:(nike|adidas|puma|asics)/;
 
 console.log(marcas.test("marca:olimpus"));
+console.log(marcas.test("marca:nikee"));
+console.log(marcas.test("marca:123"));
+console.log(marcas.test("marca:PuMa"));
