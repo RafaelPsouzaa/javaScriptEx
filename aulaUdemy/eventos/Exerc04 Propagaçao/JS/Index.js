b315274e32abcad6d4d767a73313d2d0add3b424
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-02-03 10:43:57 -0300

    atualizando tag source para script

--- aulaUdemy/eventos/Exerc04 Propagaçao/JS/Index.js
+++ aulaUdemy/eventos/Exerc04 Propagaçao/JS/Index.js
@@ -1,13 +1,18 @@
 let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
+let p = document.querySelector("p");
 
 function msg(e){
-    console.log(e);
+    console.log("clicou");
 }
 
 btn1.addEventListener("click",msg);
 
 btn2.addEventListener("click",function(e){
     console.log(e);
+});
+
+p.addEventListener("click",function(event){
+    console.log("clicou no paragrafo");
 });
 
