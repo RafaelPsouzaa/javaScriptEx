f04a0cc035610809396e2f48de4039086a612ef0
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-02-28 14:19:52 -0300

    beging JS

---
+++ aulaUdemy/jogo da velha/js/main.js
@@ -0,0 +1,6 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelectorAll("#message");
+//parou 1 min do video logica do jogo
