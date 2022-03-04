ded9f2439aebc13468b190cd7501e6de64b8bae8
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-03-04 15:28:07 -0300

    terminando csss

--- aulaUdemy/jogo da velha/js/main.js
+++ aulaUdemy/jogo da velha/js/main.js
@@ -14,5 +14,17 @@ let player2 = 0;
 //adicionando o evento de click aos boxes
 
 for(let i=0; i <boxes.length;i++){
-    
+    //quando alguem clica na caixa
+    boxes[i].addEventListener("click",function(){
+        let el; 
+
+        if(player1 == player2){
+            //x
+            el = x;
+        }else{
+            //o
+            el = o;
+        }
+    });
+
 }
