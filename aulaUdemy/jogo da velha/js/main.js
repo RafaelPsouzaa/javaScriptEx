7ab3c59edd3763fd7b3068d173551986982f2018
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-03-09 14:49:44 -0300

    block

--- aulaUdemy/jogo da velha/js/main.js
+++ aulaUdemy/jogo da velha/js/main.js
@@ -30,6 +30,10 @@ for(let i=0; i <boxes.length;i++){
             player2++;
             
         }
+        //checar quem venceu
+            checkWinCondition();
+
+
         }
         //terminou
     });
@@ -44,4 +48,19 @@ function checkEl(player1,player2){
     }
     return el;
 }
+//ve quem ganhou
+function checkWinCondition(){
+    let b1 = document.getElementById("block-1");
+    let b2 = document.getElementById("block-2");
+    let b3 = document.getElementById("block-3");
+    let b4 = document.getElementById("block-4");
+    let b5 = document.getElementById("block-5");
+    let b6 = document.getElementById("block-6");
+    let b7 = document.getElementById("block-7");
+    let b8 = document.getElementById("block-8");
+    let b9 = document.getElementById("block-9");
+    //horizontal
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0)
+}
+
 }
