94f8b4da13da99363fabdd37a56c9aa43d500844
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-08-12 10:29:13 -0300

    clear

--- aula 11/ex008.js
+++
@@ -1,6 +0,0 @@
-var velocidade = 60.5;
-if(velocidade > 60){
-    console.log(`Sua velocidade ${velocidade} Voce ultrapassou a velocidade permitida`)
-}
-
-console.log('dirija com seguranca')
--- aula 11/ex009.js
+++
@@ -1,6 +0,0 @@
-var pais = 'Franca'
-if(pais == 'Brasil'){
-    console.log('voce e Brasileiro')
-}else{
-    console.log('voce e estrangeiro')
-}
