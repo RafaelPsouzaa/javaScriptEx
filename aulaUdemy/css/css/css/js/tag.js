1e75ab24d8c9643af9e87914b4a4de230a5caef8
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-12-21 14:29:37 -0300

    css

---
+++ aulaUdemy/css/css/css/js/tag.js
@@ -0,0 +1,27 @@
+const colors ={
+    p:'#388e3c',
+    div:'#1565c0',
+    padrao:'#616161',
+    span:'#993399',
+    body:'#25b6da',
+    get(tag){
+        return this[tag] ? this[tag] : this.padrao
+    }
+}
+
+
+
+document.querySelectorAll('.tag').forEach(elemento=>{
+    const tagName = elemento.tagName.toLowerCase()
+
+    elemento.style.borderColor = colors.get(tagName)
+
+    if(!elemento.classList.contains('nolabel')){
+        const label = document.createElement('label')
+        label.style.backgroundColor = colors.get(tagName)
+        label.innerHTML = tagName
+        elemento.insertBefore(label,elemento.childNodes[0])
+
+    }
+
+})
