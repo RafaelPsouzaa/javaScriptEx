75ee49702c070c29cfa4e43ed4631f7d8f8d7cf3
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-03-27 10:37:56 -0300

    finalizado

--- JavaScript basico/exerc12/javaScript/script.js
+++ JavaScript basico/exerc12/javaScript/script.js
@@ -2,19 +2,19 @@ function carregar(){
     var msg = window.document.getElementById('msg');
     var img =window.document.getElementById('imagem')
     var data = new Date()
-    //  var hora = new data.getHours()
-    var hora = 10
-    msg.innerHTML =`agora são ${hora}horas `
+    // var hora = data.getHours()
+    var hora = 21
+   msg.innerHTML = `Agora são ${hora}horas.`
       if(hora >= 0 && hora <12){
-        //bom dia 
-        img.src = './picture/fotomanha.png'
-      }else if(hora >= 12 && < 18){
-        //Boa Tarde 
-        img.src = './picture/fototarde.png'
+      document.body.style.background = '#e2cd9f'
+        img.src = '../exerc12/picture/fotomanha.png'
+      }else if(hora >= 12 && hora < 18){
+        document.body.style.background = '#b9846f'
+        img.src = '../exerc12/picture/fototarde.png'
       }else{
          //boa noite 
-      img.src = './picture/fotonoite.png'
-      
+      img.src = '../exerc12/picture/fotonoite.png'
+      document.body.style.background = '#515154'
       }
        
-      
+    }
