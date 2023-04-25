9c07123931e1ab2470cba20cd5abe879c2ea9b21
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-04-25 08:48:06 -0300

    else change

--- JavaScript basico/modelo/index.js
+++ JavaScript basico/modelo/index.js
@@ -1,4 +1,4 @@
-function contar(){
+function contar() {
     var msg =  document.getElementById('msg');
     var ini =  document.getElementById('inicio')
     var fim =  document.getElementById('fim')
@@ -7,9 +7,23 @@ function contar(){
 
     if(ini.value.length == 0 ||fim.value.length == 0 || passo.value.length == 0){
        window.alert('faltam dados!')
-    }else{
-        msg.innerHTML = 'contando'
+    }else {
+        msg.innerHTML = 'contando: '
         let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(ini > fim){
+            for(let c =i; c <= f; c += p){
+                msg.innerHTML += `${c}  \u{1f92c}`
+            }
+            msg.innerHTML += `${c}  \u{1f3c1}`
+        }else{
+            for(let c =i; c >= f; c -= p){
+                msg.innerHTML += `${c}  \u{1f92c}`
+        }
+        msg.innerHTML += `${c}  \u{1f3c1}`
+
     }
 
-}
+    }
+    }
