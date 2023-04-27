b140df52d9d7b37e96734b1749697022b4bc138f
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-04-27 13:45:51 -0300

    react 2 parte

--- JavaScript basico/modelo/index.js
+++ JavaScript basico/modelo/index.js
@@ -6,13 +6,13 @@ function contar() {
    
 
     if(ini.value.length == 0 ||fim.value.length == 0 || passo.value.length == 0){
-       window.alert('faltam dados!')
+     msg.innerHTML = 'impossivel contar'
     }else {
         msg.innerHTML = 'contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        if(ini > fim){
+        if(i< f ){
             for(let c =i; c <= f; c += p){
                 msg.innerHTML += `${c}  \u{1f92c}`
             }
--- shopping-carter/src/App.jsx
+++ shopping-carter/src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
+import Header from './components/HEADER/HEADER.JSX';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+    <div >
+      <
     </div> 
   );   
-} 
+}
+
 export default App;
