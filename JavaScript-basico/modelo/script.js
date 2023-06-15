e488c2d3f1a58437648e7b6f0fa34c944466b771
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-06-15 11:02:30 -0300

    importando ProductCard

--- JavaScript-basico/modelo/script.js
+++ JavaScript-basico/modelo/script.js
@@ -1,6 +1,9 @@
-const a = 1;
-const b = 2;
-const c = 3;
-
-const obj1 ={atr1:a,atr2:b,atr3:c}
-console.log(obj1);
+function numeroa(a){
+   while(a < a){
+   let b = Math.floor(Math.random() * a)+1;
+   console.log(b);
+   }
+   }
+   
+   
+numeroa(10);
---
+++ JavaScript-basico/shopping-cart/src/components/Products/Products.jsx
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react';
+import '../Products/Products.css';
+import fetchProducts from '../../api/fetchProducts';
+import ProductCard from'../ProductCard/ProductCard.jsx';
+function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() =>{
+
+    fetchProducts('iphone').then((response) => {
+      setProducts(response);
+      console.log(products);
+    });//the then getting the fetchProducts response
+   
+  },[]);
+  console.log(products);
+  return ( 
+    <section className="products container">
+      <ProductCard/>
+    </section>
+  );
+}
+
+export default Products;
