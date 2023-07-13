ac7c2bc8a44a48ea1686bf0d94e6b78b7cbb60a7
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-07-13 13:47:17 -0300

    begin class 04

--- JavaScript-basico/shopping-cart/src/components/Products/Products.jsx
+++ JavaScript-basico/shopping-cart/src/components/Products/Products.jsx
@@ -2,24 +2,25 @@ import React,{useState,useEffect} from 'react';
 import '../Products/Products.css';
 import fetchProducts from '../../api/fetchProducts';
 import ProductCard from'../ProductCard/ProductCard.jsx';
+import Loading from '../loading/Loading';
 function Products() {
   const [products, setProducts] = useState([]);
+  const [loading,setLoading] = useState(true);
 
   useEffect(() =>{
 
     fetchProducts('iphone').then((response) => {
       setProducts(response);
-      console.log(products);
+      setLoading(true);
     });//the then getting the fetchProducts response
    
   },[]);
   console.log(products);
   return ( 
-    <section className="products container">
-      {
-        products.map((product) =>  <ProductCard key={product.id} data ={product}/>)
-      }
-    </section>
+    (loading ?<Loading  />:<section className="products container">
+      {products.map((product) =>  <ProductCard key={product.id} data ={product}/>)}
+    </section>)
+    
   );
 }
 
---
+++ JavaScript-basico/shopping-cart/src/components/loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../loading/Loading.css';
+import {AiOutlineLoading3Quarters} from 'react-icons/ai';
+function Loading() {
+  return <AiOutlineLoading3Quarters className='loading' />;
+}
+
+export default Loading;
