aaa445765a809a74e4dfb557e35814be44ea6eb7
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-09-27 10:16:44 -0300

    Delete JavaScript-basico/shopping-cart directory

--- JavaScript-basico/shopping-cart/src/components/loading/Loading.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../loading/Loading.css';
-import {AiOutlineLoading3Quarters} from 'react-icons/ai';
-function Loading() {
-  return <AiOutlineLoading3Quarters className='loading' />;
-}
-
-export default Loading;
